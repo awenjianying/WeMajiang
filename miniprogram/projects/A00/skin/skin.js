2c02a461dadf5f8f3770eccefb410990c48edb2a
newboys111 <newboys111@foxmail.com>
2022-03-18 13:30:24 +0800

    Initial Commit

---
+++ miniprogram/projects/A00/skin/skin.js
@@ -0,0 +1,44 @@
+module.exports = {
+	PID: 'A00', //会议室
+
+	NAV_COLOR: '#ffffff',
+	NAV_BG: '#d7000f',
+	cate2: false,
+
+	MEET_NAME: '预约',
+
+	MENU_ITEM: ['首页', '预约日历', '我的'], // 第1,4,5菜单
+
+	NEWS_CATE: '1=预约规则',
+	MEET_TYPE: '1=办公室预约|leftbig2,2=会议室预约|leftbig3',
+
+	DEFAULT_FORMS: [{
+			type: 'line',
+			title: '姓名',
+			desc: '请填写您的姓名',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		},
+		{
+			type: 'line',
+			title: '手机',
+			desc: '请填写您的手机号码',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		}
+	]
+}
